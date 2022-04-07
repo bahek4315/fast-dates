39bf1f3615647745c38b39f61ad5ecf5d9e50035
bahek4315 <bahek4315@yandex.ru>
2022-04-07 13:05:41 +0300

    changed handleBookmark in App.js, less code

--- src/app/App.js
+++ src/app/App.js
@@ -12,13 +12,9 @@ const App = () => {
     };
 
     const handleBookmark = (id) => {
-        const currentState = Array.from(users);
-        const foundPosition = currentState.indexOf(users.find(item => item._id === id));
-        if (currentState[foundPosition].bookmark) {
-            currentState[foundPosition].bookmark = false;
-        } else {
-            currentState[foundPosition].bookmark = true;
-        }
+        const currentState = [...users];
+        const foundPosition = currentState.findIndex(item => item._id === id);
+        currentState[foundPosition].bookmark = !currentState[foundPosition].bookmark;
         setUsers(currentState);
     };
     
