3d9b81b4b39340f0c25abd5cd353a68307b4bf75
bahek4315 <bahek4315@yandex.ru>
2022-04-07 16:11:22 +0300

    changed usersList.jsx deleted checking if users is empty

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -5,33 +5,31 @@ const Users = (props) => {
     let users = props.usersList;
 
     const renderTable = () => {
-        if (users.length !== 0) {
-            return (
-                <>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Имя</th>
-                                <th>Качества</th>
-                                <th>Профессия</th>
-                                <th>Встретился, раз</th>
-                                <th>Оценка</th>
-                                <th>Избранное</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {users.map((user) => (
-                                    <tr key={user._id} >
-                                        <User {...user} onDelete={props.onDelete} onBookmark={props.onBookmark}/>
-                                    </tr>
-                                )
-                            )}
-                        </tbody>
-                    </table>
-                </>
-            );
-        }
+        return (
+            <>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Имя</th>
+                            <th>Качества</th>
+                            <th>Профессия</th>
+                            <th>Встретился, раз</th>
+                            <th>Оценка</th>
+                            <th>Избранное</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                                <tr key={user._id} >
+                                    <User {...user} onDelete={props.onDelete} onBookmark={props.onBookmark}/>
+                                </tr>
+                            )
+                        )}
+                    </tbody>
+                </table>
+            </>
+        );
     };
     
     return (
