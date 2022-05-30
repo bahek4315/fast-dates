dfd17f6feee0ccbcdbfdc26a20570249a2776c35
bahek4315 <bahek4315@yandex.ru>
2022-05-30 17:38:34 +0300

    deleted console.loh

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -12,7 +12,6 @@ const UserPage = () => {
         () =>
             api.users.getById(userId).then((data) => {
                 setUser(data);
-                console.log(user);
             }),
         []
     );
